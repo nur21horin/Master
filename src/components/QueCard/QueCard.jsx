4f7dc81b7104c51bc45d117aa7b5da66a7cf63d5
Nur Mohammod <nurm98371@gmail.com>
2025-08-15 04:25:57 +0600

    Que Card Section

--- src/components/QueCard/QueCard.jsx
+++ src/components/QueCard/QueCard.jsx
@@ -1,18 +1,57 @@
-import React from 'react' 
-import './QueCard.css'
-const QueCard = () => {
-  return (
-    <div className='queCard mx-auto text-center bg-red-950 border-2 border-amber-950 rounded-lg w-2/3 shadow-amber-600 mt-8'>
-      <h1>This is queCard</h1>
-      <h1>Q-126</h1>
-      <p><span>Band 8-9 Sample Answer :</span>"YouTube has revolutionized the way people access educational content, making learning more accessible and interactive.
-      With countless channels dedicated to various subjects, from academic tutorials to skill-based learning, YouTube has become a go-to resource for students,
-      Professionals ,and lifelong learners. Educational content on YouTube ranges from short explainer videos to in-depth lectures and courses,
-      catering to diverse learning needs. The advantage of YouTube as an educational tool are numerous. It offers the flexibility to learn at one's own pace,
-      access to a vast array of topics, and the ability to revisit content as need.
-      The visual and auditory elements also enhance understanding and retention. </p>
-    </div>
-  )
-}
 
-export default QueCard
+import React, { useEffect, useState } from "react";
+import "./QueCard.css";
+import { FaPlane } from "react-icons/fa";
+
+const QueCard = () => {
+  const [cards, setCards] = useState([]);
+  const [clicks, setClicks] = useState({});
+
+  const handleClick = (id) => {
+    setClicks((prev) => ({
+      ...prev,
+      [id]: (prev[id] || 0) + 1,
+    }));
+  };
+
+  useEffect(() => {
+    fetch("/QueCard.json")
+      .then((res) => res.json())
+      .then((data) => setCards(data))
+      .catch((err) => console.error("Error loading JSON:", err));
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-3 m-3.5 font-serif">
+      {cards.map((card) => {
+        const count = clicks[card.id] || 0;
+
+        return (
+          <div
+            key={card.id}
+            className="w-2/3 mx-auto text-center border-2 bg-white text-gray-600 text-2xl rounded-2xl shadow-amber-300"
+            onClick={() => handleClick(card.id)}
+          >
+            {count < 3 ? (
+              <div>
+                <h2 className="font-bold text-3xl">Q-{card.id}</h2>
+                <h3 className="font-bold text-3xl">Question: {card.title}</h3>
+                <p>Answer: {card.answer}</p>
+              </div>
+            ) : (
+              <div className="relative h-32 overflow-hidden">
+                <FaPlane
+                  size={80}
+                  className="absolute text-blue-600 flying-plane"
+                />
+                <p className="text-xl mt-20">Taking off to success! 🛫</p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default QueCard;
