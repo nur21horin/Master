8c987722be531bc8950c518ab28b95cde7889410
Nur Mohammod <nurm98371@gmail.com>
2025-08-16 08:53:18 +0600

    vocab section

--- src/components/Vocabulary/Vocabulary.jsx
+++ src/components/Vocabulary/Vocabulary.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLoaderData } from 'react-router';
+import { NavLink, useLoaderData } from 'react-router';
 
 const Vocabulary = () => {
 
@@ -33,15 +33,19 @@ const Vocabulary = () => {
 
   return (
     <div className='text-center flex-wrap flex flex-col items-center overflow-x-auto gap-y-4 justify-center h-auto w-full'>
-    <h1>Welcome in Vocab Zone..</h1>
+    <h1 className='text-1xl font-serif mt-3.5'>You can add your favourite words with meaning</h1>
    
       <form className='items-center justify-center ' onSubmit={handleAddWord}>
+      <h1 className='text-2xl text-left font-semibold font-serif mb-1.5 '>English word</h1>
         <input type='text' name='text' placeholder='Enter your word' id='' className='bg-amber-50 text-opacity-[1] mb-[20px] text-2xl w-[550px] text-black rounded-lg ' />
         <br/>
-         <input type='text' name='bengalitext' placeholder='Meaning of the given word' id='' className='bg-amber-50 text-2xl w-[550px] text-black rounded-lg ' />
+        <h1 className='text-2xl font-semibold font-serif text-left mb-1.5'>Bengali meaning</h1>
+         <input type='text' name='bengalitext' placeholder='Meaning of the given word' id='' className='bg-amber-50 placeholder-opacity-[0.01] placeholder-gray-500 text-2xl w-[550px] text-black rounded-lg ' />
         <br/>
-        <input type='submit' value="Enter" className='text-2xl font-serif bg-emerald-900 rounded-lg mt-4 w-[150px] h-auto '/>
+        <input type='submit' value="Enter your word" className='text-2xl hover:bg-blue-700 transition duration-300 w-full font-serif bg-emerald-900 rounded-lg mt-4 h-auto '/>
       </form>
+      <NavLink to={"/showvocab"} className='text-2xl hover:bg-blue-700 transition duration-300 w-[550px] font-serif bg-emerald-900 rounded-lg mt-4 h-auto'>Show Vocabulary</NavLink>
+
     </div>
   )
 }
